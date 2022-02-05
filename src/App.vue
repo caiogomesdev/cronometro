be81272ab1bb228f8ed3a3d016e12c604a106a28
Caio Gomes <caiogame100@gmail.com>
2022-02-05 15:44:03 -0300

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="img" src="./assets/cronometro.png" />
+    <a class="timer">{{zfill(hour)}}:{{zfill(min)}}:{{zfill(sec)}}</a>
+
+    <div class="btns">
+      <button class="btn" @click="play">{{isPlaying !== null ? "PAUSAR" :"VAI" }}</button>
+      <button class="btn" @click="clear">LIMPAR</button>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      sec: 0,
+      min: 0,
+      hour: 0,
+      isPlaying: null,
+
+    }
+  },
+  methods: {
+    zfill(number){
+      return number.toString().padStart(2,0)
+    },
+    play(){
+      
+      if(this.isPlaying === null){
+        this.playing()
+        this.isPlaying = setInterval(()=> this.playing(), 1000)
+      }
+      else{
+        clearInterval(this.isPlaying);
+        this.isPlaying = null;
+        this.pause();
+      }
+    },
+    playing(){
+      this.sec++
+      if(this.sec >= 60){
+        this.sec = 0;
+        this.min++;
+      }
+      if(this.min >= 60){
+        this.min = 0;
+        this.hour++;
+      }
+
+    },
+    pause(){
+      console.log("Pause")
+
+    },
+    clear(){
+      this.sec = 0;
+      this.min = 0;
+      this.hour = 0;
+
+    }
+
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+#app{
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.img{
+  width: 420px;
+  height: 420px;
+  padding-top: 100px;
+
+}
+.timer{
+  color: #fff;
+  font-size: 70px;
+  margin-top: -210px;
+}
+.btns{
+  margin-top: 155px;
+  display: flex;
+}
+.btn{
+  -webkit-user-select: none;
+  -moz-user-select: none;
+
+  width: 150px;
+  background-color: #fff;
+  font-size: 20px;
+  border: none;
+  border-radius: 5px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px 7px;
+  padding: 6px;
+  cursor: pointer;
+  transition: all ease-in-out .3s;
+}
+.btn:hover{
+  opacity: 0.8;
 }
 </style>
